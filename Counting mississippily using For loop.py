0d3e1cbeab6dfa3abd201eb25f964ff01fb59c69
Anton Gabia <tonygabuay@gmail.com>
2025-12-01 22:25:14 +0800

    Second day learning Python

---
+++ Counting mississippily using For loop.py
@@ -0,0 +1,13 @@
+import time
+
+# Write a for loop that counts to five.
+    # Body of the loop - print the loop iteration number and the word "Mississippi".
+    # Body of the loop - use: time.sleep(1)
+    
+for count in range (1, 6):
+    time.sleep(1)
+    print(f"{count} Mississippi")
+    
+
+# Write a print function with the final message.
+
---
+++ for loop exercise.py
@@ -0,0 +1,15 @@
+#For Loop
+
+#Traverse the value in the range() function
+#for i in range(100):
+#    print(f"The value of i is {i}")
+
+
+#The range function can accept 2 arguments. The first one is the starting value. The second is the end value (Won't be counted).
+#for i in range(2, 8):
+#    print(f"The value of i is {i}")
+
+#It can also accepts 3 arguments
+#for i in range(2, 8, 3): #Means start with 2, ends i 8,  and count by 3 (increment value).
+#    print(f"The value of i is {i}")
+
